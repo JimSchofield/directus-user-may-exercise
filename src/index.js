aea827459171ef28be4b6eb91f8928a83d2b7d43
JimSchofield <oldcoyote@gmail.com>
2021-02-08 21:18:26 -0600

    Initial framework in place

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * A function that decides when a user has permission to 
+ * do an action
+ * @param {Array} user permission object
+ * @param {Array} collection object
+ * @param {string} may be one of ['create', 'update', 'read', 'delete']
+ */
+function mayUser(userPermissions, collections, actionType) {
+  // your logic goes here
+}
+
+module.exports = mayUser;
---
+++ src/tests/test1.test.js
@@ -0,0 +1,5 @@
+const mayUser = require('../index');
+
+test('works', () => {
+  expect(mayUser()).toBe(true);
+})
